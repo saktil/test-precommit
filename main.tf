88cd4e15469324caaed0bb8fe8071c7124ea76f8
saktil <leosaragih001@gmil.com>
2022-08-01 14:15:43 +0700

    mantap

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ resource "aws_vpc" "my_vpc" {
 
   tags = {
 
+<<<<<<< HEAD
     yor_trace            = "52f6a1d7-7e70-4428-848d-a0a3737bf999"
+=======
+>>>>>>> 5e5418b (update1)
     git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
     git_file             = "main.tf"
     git_last_modified_at = "2022-08-01 06:46:57"
@@ -11,6 +14,10 @@ resource "aws_vpc" "my_vpc" {
     git_modifiers        = "leosaragih001"
     git_org              = "saktil"
     git_repo             = "test-precommit"
+<<<<<<< HEAD
+=======
+    yor_trace            = "3a675c10-2bc4-4620-8a3a-9ca10b829fff"
+>>>>>>> 5e5418b (update1)
   }
 }
 
@@ -21,7 +28,10 @@ resource "aws_subnet" "my_subnet" {
 
   tags = {
 
+<<<<<<< HEAD
     yor_trace            = "61f3edfa-efc9-431f-8057-c643bfa5a16b"
+=======
+>>>>>>> 5e5418b (update1)
     git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
     git_file             = "main.tf"
     git_last_modified_at = "2022-08-01 06:46:57"
@@ -29,6 +39,10 @@ resource "aws_subnet" "my_subnet" {
     git_modifiers        = "leosaragih001"
     git_org              = "saktil"
     git_repo             = "test-precommit"
+<<<<<<< HEAD
+=======
+    yor_trace            = "cf512c6b-38b8-496e-b989-eb706aad6753"
+>>>>>>> 5e5418b (update1)
   }
 }
 
@@ -38,7 +52,10 @@ resource "aws_network_interface" "foo" {
 
   tags = {
 
+<<<<<<< HEAD
     yor_trace            = "6e420c66-56c4-474f-b39f-ed7c4699773c"
+=======
+>>>>>>> 5e5418b (update1)
     git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
     git_file             = "main.tf"
     git_last_modified_at = "2022-08-01 06:46:57"
@@ -46,6 +63,10 @@ resource "aws_network_interface" "foo" {
     git_modifiers        = "leosaragih001"
     git_org              = "saktil"
     git_repo             = "test-precommit"
+<<<<<<< HEAD
+=======
+    yor_trace            = "c6b00f19-6b71-4895-8f45-6659f166f472"
+>>>>>>> 5e5418b (update1)
   }
 }
 
@@ -59,7 +80,10 @@ resource "aws_instance" "foo" {
   }
   tags = {
 
+<<<<<<< HEAD
     yor_trace            = "362a29fb-d7ef-4416-b371-92bf3181c459"
+=======
+>>>>>>> 5e5418b (update1)
     git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
     git_file             = "main.tf"
     git_last_modified_at = "2022-08-01 06:46:57"
@@ -67,6 +91,10 @@ resource "aws_instance" "foo" {
     git_modifiers        = "leosaragih001"
     git_org              = "saktil"
     git_repo             = "test-precommit"
+<<<<<<< HEAD
+=======
+    yor_trace            = "9b8b138e-73fa-4908-af59-f5cf984a0c95"
+>>>>>>> 5e5418b (update1)
   }
 }
 
