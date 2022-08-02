f559fca9c393cf84c2124ecc0ae933a318e062b0
saktil <leosaragih001@gmil.com>
2022-08-02 15:09:20 +0700

    update-3

--- main.tf
+++ main.tf
@@ -3,14 +3,8 @@ resource "aws_vpc" "my_vpc" {
 
   tags = {
 
-    git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-08-01 06:46:57"
-    git_last_modified_by = "leosaragih001@gmil.com"
-    git_modifiers        = "leosaragih001"
-    git_org              = "saktil"
-    git_repo             = "test-precommit"
-    yor_trace            = "89669a2c-fc6f-4bcb-aad9-33402c485e85"
+
+    yor_trace = "4c9d35db-dc55-4210-8808-d495de54555f"
   }
 }
 
@@ -28,7 +22,7 @@ resource "aws_subnet" "my_subnet" {
     git_modifiers        = "leosaragih001"
     git_org              = "saktil"
     git_repo             = "test-precommit"
-    yor_trace            = "fe55e8fe-64e2-494a-a511-bd616fa8e58a"
+    yor_trace            = "a2bb2812-b29f-4c19-b311-cd7118c84c5f"
   }
 }
 
@@ -38,14 +32,8 @@ resource "aws_network_interface" "foo" {
 
   tags = {
 
-    git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-08-01 06:46:57"
-    git_last_modified_by = "leosaragih001@gmil.com"
-    git_modifiers        = "leosaragih001"
-    git_org              = "saktil"
-    git_repo             = "test-precommit"
-    yor_trace            = "4e36f768-532b-4d82-aa1a-112621bbcf9d"
+
+    yor_trace = "10d1ff62-8318-4ebc-ac74-1fdc2321bcf9"
   }
 }
 
@@ -59,14 +47,8 @@ resource "aws_instance" "foo" {
   }
   tags = {
 
-    git_commit           = "8b31c449cb19e78963f8dbb595132101610dda70"
-    git_file             = "main.tf"
-    git_last_modified_at = "2022-08-01 06:46:57"
-    git_last_modified_by = "leosaragih001@gmil.com"
-    git_modifiers        = "leosaragih001"
-    git_org              = "saktil"
-    git_repo             = "test-precommit"
-    yor_trace            = "2f989939-d84e-49d4-aceb-b40f4cded2d5"
+
+    yor_trace = "c9129b11-3009-4904-8335-ed9f6c04164f"
   }
 }
 
